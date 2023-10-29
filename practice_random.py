10fad183d45cd58a0c7f44cbad99494013ca4ba3
Hossain Shadman <hs@HossainMP.local>
2023-10-29 14:53:03 -0500

    practice work

--- practice_random.py
+++ practice_random.py
@@ -2,11 +2,11 @@ class Car:
     def __init__(self,color,model,year):
         self._color = color
         self.__model = model
-        self.__year__ = year
+        self.__year_ = year
 
 c = Car("red", "Toyota", 2020)
 
 c._Car_color = "green"
 #c.__model = 'xyz'
 
-print(c._color, c._Car_color, c.__year__)
+print(c._color, c._Car_color, c._Car__year_)
--- worksheet_2.py
+++ worksheet_2.py
@@ -1,2 +1,73 @@
+from enum import Enum
+from itertools import product
+
 #Problem 1
-#valid, no, no, yes, yes, no
+
+#got those wrong. trailing underscore more than 2 prevents name mangling
+#you canc create new attributes for instances of a class
+
+#Problem 2
+class suits(Enum):
+      SPADES   = 3
+      HEARTS   = 2
+      DIAMONDS = 1
+      CLUBS    = 0
+
+class PlayingCard:
+    def __init__(self,rank,suit):
+        if rank in [i for i in range(2,15)]:
+            self._rank = rank
+        elif rank == 'Jack':
+	        self._rank = 11
+        elif rank == 'Queen':
+	        self._rank = 12
+        elif rank == 'King':
+    	    self._rank=13
+        elif rank == 'Ace':
+	        self._rank=14
+        else:
+            raise ValueError('ERROR. Ranks can range from 2 to 14 inclusive or be one of these choices: Jack, Queen, King or Ace')
+            
+        if suit == 'SPADES':
+            self._suit = suits.SPADES.value
+        elif suit == 'HEARTS':
+            self._suit = suits.HEARTS.value
+        elif suit == 'DIAMONDS':
+            self._suit = suits.DIAMONDS.value            
+        elif suit == 'CLUBS':
+            self._suit = suits.CLUBS.value            
+        else:
+            raise ValueError('ERROR. Suits can only be SPADES, HEARTS, DIAMONDS or CLUBS') 
+    def get_rank(self):
+         return self._rank
+    def get_suit(self):
+         return self._suit
+
+
+class Player:
+    def __init__(self,name,hand):        
+        self._name = name
+        self._hand = []
+    def set_hand(self, hand):
+        if (self._hand)>2:
+            raise ValueError('ERROR, more than 2 hands')
+        else:
+            return self._hand.append(hand)
+    def get_name(self):
+        return self._name
+    def get_hand(self):
+        return self._hand
+    def strongest_hand():
+        
+class Deck:
+    def __init__(self):
+        ranks = range(2,15)
+        suits = ['SPADES','HEARTS','DIAMONDS','CLUBS']
+        card_combinations = list(product(ranks,suits))
+        self._cards = []
+        for card in card_combinations:
+            self._cards.append(PlayingCard(card[0],card[1]))
+    def get_cards(self):
+         return self._cards
+
+#Problem 3
