85c7a7931b05a1373f0e67acf1816c1604802f05
Hossain Shadman <hs@HossainMP.local>
2023-10-29 17:30:28 -0500

    scratchwork

--- practice_random.py
+++ practice_random.py
@@ -1,14 +1,18 @@
 class Apple:
     food_type = "fruit"
-    calories_per_100_gram = 50
+    calories = 50
     def __init__(self, color, weight):
         self.color = color
-        self.weight =weight
-    def get_calories(self):
-        return (self.weight/100)* self.calories_per_100_gram        
+        self.weight = weight
 
-a1 = Apple('red', 100)
-a2 = Apple('green', 150)
+    def set_color(self, color):
+        print(f'self has id {id(self)}')
+        print(f'class of self has id {id(self.__class__)}')
+        #self.color = color
+        self.__class__.food_type = "sweet fruit"
+a = Apple("red", 100)
+print(f'id of instance a is {id(a)}')
+print(f'id of class Apple is {id(Apple)}')
 
-print(f"Apple a1 has {a1.get_calories()} calories")
-print(f"Apple a2 has {a2.get_calories()} calories")
+a.set_color("green")
+print(a.food_type)
